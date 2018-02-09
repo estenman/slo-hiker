a005bc1ce94493983a8083c55d64cb446f202031
Ericka Stenman <elstenman@gmail.com>
2018-02-08 19:18:45 -0800

    Added trail sorting functionality

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,6 +1,23 @@
 (function() {
   function HomeCtrl(Trails) {
     this.trails = Trails.getAllTrails();
+
+    this.changeFilterTo = function(item) {
+      this.filter = item;
+    }
+
+    this.getFilter = function(){
+      switch (this.filter) {
+        case 'difficulty':
+          return 'difficulty';
+        case 'timeToHike':
+          return 'timeToHike';
+        case 'length':
+          return 'length';
+        default:
+          return 'trailName';
+      }
+    }
   }
 
   angular
--- app/scripts/filters/trailsort.js
+++ app/scripts/filters/trailsort.js
@@ -1,13 +1,14 @@
 (function(){
   function trailsort () {
-    return function(itemSorting) {
-      if itemSorting = difficulty {
-
-      } else if itemSorting = time {
-
-      } else if itemSorting = length {
-        
-      }
+    return function(items, field) {
+      var filtered = [];
+      angular.forEach(items, function(item) {
+        filtered.push(item);
+      });
+      filtered.sort(function (a, b) {
+        return (a[field] > b[field] ? 1 : -1);
+      });
+      return filtered;
     };
   }
 
