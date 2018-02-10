f363f20211ae28216f79cf635c0c44db0bd05f11
Ericka Stenman <elstenman@gmail.com>
2018-02-10 15:42:39 -0800

    Cleanup for AngularGM to make it DRY

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,6 +1,29 @@
 (function() {
   function HomeCtrl(Trails, $rootScope, $scope) {
 
+//Retrieves all trails in the Trail service
+    this.trails = Trails.getAllTrails();
+
+//Used to indicate what the user selected to sort by
+    this.changeFilterTo = function(item) {
+      this.filter = item;
+    }
+
+//Returns to the views filter what the user choose to sort by.
+    this.getFilter = function(){
+      switch (this.filter) {
+        case 'difficulty':
+          return 'difficulty';
+        case 'timeToHike':
+          return 'timeToHike';
+        case 'length':
+          return 'length';
+        default:
+          return 'trailName';
+      }
+    }
+
+//Provides information for map display
     this.options = {
       map: {
         center: new google.maps.LatLng(35.282752, -120.659616),
@@ -15,79 +38,10 @@
       }
     };
 
-    this.hikingTrails = [
-      {
-        id: 0,
-        name: 'Cerro San Luis',
-        img: 'http://www.thetrackerfoundation.org/Images/MountRainier_SM.jpg',
-        elevationMeters: 4392,
-        location: {
-          lat: 35.2748481,
-          lng: -120.6767364
-        }
-      },
-      {
-        id: 1,
-        name: 'Irish Hills Mariposa King',
-        img: 'http://www.destination360.com/north-america/us/washington/images/s/washington-mt-baker-ski.jpg',
-        elevationMeters: 3287,
-        location: {
-          lat: 35.254311,
-          lng: -120.69525
-        }
-      },
-      {
-        id: 2,
-        name: 'Irish Hills Froom Creek',
-        img: 'http://www.rhinoclimbs.com/Images/Glacier.9.jpg',
-        elevationMeters: 3207,
-        location: {
-          lat: 35.254311,
-          lng: -120.69525
-        }
-      },
-      {
-        id: 3,
-        name: 'Islay Hill',
-        img: 'http://www.destination360.com/north-america/us/washington/images/s/washington-mt-baker-ski.jpg',
-        elevationMeters: 3287,
-        location: {
-          lat: 35.243296,
-          lng: -120.62713
-        }
-      },
-      {
-        id: 4,
-        name: 'Poly Canyon',
-        img: 'http://www.destination360.com/north-america/us/washington/images/s/washington-mt-baker-ski.jpg',
-        elevationMeters: 3287,
-        location: {
-          lat: 35.30445,
-          lng: -120.65845
-        }
-      },
-      {
-        id: 5,
-        name: 'Felsman Loop',
-        img: 'http://www.destination360.com/north-america/us/washington/images/s/washington-mt-baker-ski.jpg',
-        elevationMeters: 3287,
-        location: {
-          lat: 35.30433,
-          lng: -120.685438
-        }
-      },
-      {
-        id: 6,
-        name: 'Bishop Peak',
-        img: 'http://www.destination360.com/north-america/us/washington/images/s/washington-mt-baker-ski.jpg',
-        elevationMeters: 3287,
-        location: {
-          lat: 35.30115,
-          lng: -120.68916
-        }
-      }
-    ];
+//map trail info
+    this.hikingTrails = Trails.trailArray();
 
+//Used to change the marker color when clicked on and to update indicator if it is selected.
     this.getHikingTrailOpts = function(trl) {
      return angular.extend(
        { title: trl.name },
@@ -96,8 +50,8 @@
       );
     };
 
+//Used to display the trail information when the marker is selected.
     this.selectHikingTrail = function(trl) {
-
       if ($scope.trl) {
         $scope.trl.selected = false;
       }
@@ -107,24 +61,7 @@
       $rootScope.$broadcast('gmMarkersUpdate', 'this.hikingTrails');
     };
 
-    this.trails = Trails.getAllTrails();
 
-    this.changeFilterTo = function(item) {
-      this.filter = item;
-    }
-
-    this.getFilter = function(){
-      switch (this.filter) {
-        case 'difficulty':
-          return 'difficulty';
-        case 'timeToHike':
-          return 'timeToHike';
-        case 'length':
-          return 'length';
-        default:
-          return 'trailName';
-      }
-    }
   }
 
   angular
