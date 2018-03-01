1a3f38dbd88c5bc0e8ff4a645ce1020b9827cffd
Ericka Stenman <elstenman@gmail.com>
2018-02-28 20:24:30 -0800

    Add maps and picture

--- app/scripts/controllers/TrailCtrl.js
+++ app/scripts/controllers/TrailCtrl.js
@@ -2,20 +2,9 @@
   function TrailCtrl(Trails, $stateParams, $state) {
     var trailBeingPassed = $stateParams.trailId;
     this.trailData = Trails.getTrail(trailBeingPassed);
-
-    /*this.run = function($state, $stateParams) {
-      var trailBeingPassed = $stateParams.trailId;
-      this.trailData = Trails.getTrail(trailBeingPassed);
-    }*/
   }
 
   angular
     .module('sloHiker')
     .controller('TrailCtrl', ['Trails', '$stateParams', '$state', TrailCtrl]);
-
-    /*.run(['$state', '$stateParams',
-    function($state, $stateParams) {
-      var trailBeingPassed = $stateParams.trailId;
-      this.trailData = Trails.getTrail(trailBeingPassed);
-    }]); error unexpected . */
 })();
